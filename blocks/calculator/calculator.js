dace726d16913cdd89122af2a52da62af0b678f4
VL <lakkamra@adobe.com>
2024-07-12 16:06:12 -0400

    calculator component

---
+++ blocks/calculator/calculator.js
@@ -0,0 +1,146 @@
+export default async function decorate(block) {
+    block.innerHTML = '<script>let first_number = "0";\n' +
+        'let second_number = "0";\n' +
+        'let result = "0";\n' +
+        'let current_operator;\n' +
+        'let evaluation = [];\n' +
+        'const screen = document.querySelector(".screen");\n' +
+        'const keyboard = document.querySelector(".keyboard");\n' +
+        'keyboard.addEventListener(\'click\', function(e) {\n' +
+        '    e.stopImmediatePropagation()\n' +
+        '    onButtonPress(e);\n' +
+        '});\n' +
+        'function onButtonPress (e) {\n' +
+        '    switch(e.target.getAttribute(\'data-button-type\')) {\n' +
+        '        case "digit":\n' +
+        '            AssignNumber(e)\n' +
+        '            break;\n' +
+        '        case "operator":\n' +
+        '            AssignOperation(e)\n' +
+        '            break;\n' +
+        '    }\n' +
+        '    Render(e);\n' +
+        '}\n' +
+        'function AssignNumber(e) {\n' +
+        '    if(evaluation.length <= 1) {\n' +
+        '        first_number = first_number == "0" \n' +
+        '            ? e.target.getAttribute("data-value")\n' +
+        '            : first_number + e.target.getAttribute("data-value")\n' +
+        '        if(evaluation.length == 1) evaluation.shift();\n' +
+        '        evaluation.push(first_number)\n' +
+        '        result = first_number;\n' +
+        '        return;\n' +
+        '    }\n' +
+        '    if (evaluation.length >= 2) {\n' +
+        '        second_number = second_number == "0"\n' +
+        '            ? e.target.getAttribute("data-value")\n' +
+        '            : second_number + e.target.getAttribute("data-value");\n' +
+        '        if(evaluation.length == 3) evaluation.pop();\n' +
+        '        evaluation.push(second_number);\n' +
+        '        result = second_number;\n' +
+        '    }\n' +
+        '}\n' +
+        'function AssignOperation(e) {\n' +
+        '    current_operator = e.target.getAttribute(\'data-value\');\n' +
+        '    // Exclusive operations that can be performed with one number, in the case of clear it can be executed even when the evaluation array is empty\n' +
+        '    if(current_operator == "%" || current_operator == "+/-" || current_operator == "clear" || current_operator == "=") return Operate();\n' +
+        '    if(evaluation.length == 3) Operate();\n' +
+        '    if(evaluation.length == 2) evaluation.pop();\n' +
+        '    evaluation.splice(1, 1, current_operator);\n' +
+        '}\n' +
+        'function Operate() { \n' +
+        '    if(current_operator == "%" && evaluation.length) {\n' +
+        '        let number = parseInt(evaluation[evaluation.length - 1])\n' +
+        '        result =  (number / 100).toString();\n' +
+        '        evaluation.splice(evaluation.length - 1, 1, result);\n' +
+        '        return;\n' +
+        '    }\n' +
+        '    if(current_operator == "+/-" && evaluation.length) {\n' +
+        '        result = (evaluation[evaluation.length - 1] * -1).toString();\n' +
+        '        evaluation.splice(evaluation.length - 1, 1, result);\n' +
+        '        return;\n' +
+        '    }\n' +
+        '    if(current_operator == "clear") {\n' +
+        '        if(evaluation.length <= 2) {\n' +
+        '            first_number = "0";\n' +
+        '            evaluation = [];\n' +
+        '            result = "0";\n' +
+        '            return;\n' +
+        '        }\n' +
+        '        if(evaluation.length == 3) {\n' +
+        '            second_number = "0";\n' +
+        '            evaluation = [first_number.toString()]\n' +
+        '            result = first_number.toString();\n' +
+        '            return;\n' +
+        '        }\n' +
+        '    }\n' +
+        '    if(evaluation.length == 3) {\n' +
+        '        result = (eval(evaluation.join().replace(/,/g, ""))).toString();\n' +
+        '        first_number = result;\n' +
+        '        second_number = "0";\n' +
+        '        evaluation = [first_number]\n' +
+        '    }\n' +
+        '}\n' +
+        'function Render(e) {\n' +
+        '    const clear_button = document.querySelector(\'div[data-value="clear"]\');\n' +
+        '    let new_operator_button = e.target;\n' +
+        '    let last_operator_button = document.querySelector(\'.selected_operation\');\n' +
+        '    last_operator_button ? last_operator_button.classList.remove(\'selected_operation\') : null;\n' +
+        '    new_operator_button ? new_operator_button.classList.add(\'selected_operation\') : null;\n' +
+        '    // change screen\'s font-size\n' +
+        '    switch(result.toString().length) {\n' +
+        '        case 7:\n' +
+        '            screen.style.fontSize = "4.7rem"\n' +
+        '            break;\n' +
+        '        case 8:\n' +
+        '            screen.style.fontSize = "4.1rem"\n' +
+        '            break;\n' +
+        '        case 9: \n' +
+        '            screen.style.fontSize = "3.65rem"\n' +
+        '            break\n' +
+        '    }\n' +
+        '    if(result.toString().length > 9) {\n' +
+        '        screen.textContent = parseFloat(result).toPrecision(3);\n' +
+        '    } else {\n' +
+        '        screen.textContent = result;\n' +
+        '    }\n' +
+        '    evaluation.length == "0"\n' +
+        '        ? clear_button.textContent = \'AC\'\n' +
+        '        : clear_button.textContent = \'C\'\n' +
+        '}</script><div class="calculator">\n' +
+        '    <div class="screen">\n' +
+        '        0\n' +
+        '    </div>\n' +
+        '    <div class="keyboard">\n' +
+        '        <div class="row">\n' +
+        '            <div class="sub-col" data-button-type="operator" data-value="clear" id="clear_button">AC</div>\n' +
+        '            <div class="sub-col" data-button-type="operator" data-value="+/-">+/-</div>\n' +
+        '            <div class="sub-col" data-button-type="operator" data-value="%">%</div>\n' +
+        '            <div class="sub-col" data-button-type="operator" data-value="/">÷</div>\n' +
+        '        </div>\n' +
+        '        <div class="row">\n' +
+        '            <div class="sub-col" data-button-type="digit" data-value="7">7</div>\n' +
+        '            <div class="sub-col" data-button-type="digit" data-value="8">8</div>\n' +
+        '            <div class="sub-col" data-button-type="digit" data-value="9">9</div>\n' +
+        '            <div class="sub-col" data-button-type="operator" data-value="*">x</div>\n' +
+        '        </div>\n' +
+        '        <div class="row">\n' +
+        '            <div class="sub-col" data-button-type="digit" data-value="4">4</div>\n' +
+        '            <div class="sub-col" data-button-type="digit" data-value="5">5</div>\n' +
+        '            <div class="sub-col" data-button-type="digit" data-value="6">6</div>\n' +
+        '            <div class="sub-col" data-button-type="operator" data-value="-">-</div>\n' +
+        '        </div>\n' +
+        '        <div class="row">\n' +
+        '            <div class="sub-col" data-button-type="digit" data-value="1">1</div>\n' +
+        '            <div class="sub-col" data-button-type="digit" data-value="2">2</div>\n' +
+        '            <div class="sub-col" data-button-type="digit" data-value="3">3</div>\n' +
+        '            <div class="sub-col" data-button-type="operator" data-value="+">+</div>\n' +
+        '        </div>\n' +
+        '        <div class="row">\n' +
+        '            <div class="sub-col" data-button-type="digit" data-value="0">0</div>\n' +
+        '            <div class="sub-col" data-button-type="digit" data-value=".">,</div>\n' +
+        '            <div class="sub-col" data-button-type="operator" data-value="=">=</div>\n' +
+        '        </div>\n' +
+        '    </div>\n' +
+        '</div>';
+}
